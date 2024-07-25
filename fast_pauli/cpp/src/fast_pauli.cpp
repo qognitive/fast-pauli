5f38586c3edd3604c8cdc7556c9c703078877719
Eugene Rublenko <16805621+stand-by@users.noreply.github.com>
2024-07-24 20:26:38 -0400

    Draft bindings for PauliString

--- fast_pauli/cpp/src/fast_pauli.cpp
+++ fast_pauli/cpp/src/fast_pauli.cpp
@@ -43,5 +43,60 @@ PYBIND11_MODULE(_fast_pauli, m) {
         return self * rhs;
       });
 
+  py::class_<fp::PauliString>(m, "PauliString")
+      .def(py::init<>())
+      .def(py::init([](std::vector<fp::Pauli> paulis) {
+             return fp::PauliString(paulis);
+           }),
+           "paulis"_a)
+      .def(py::init<std::string const &>(), "string"_a)
+      .def_property_readonly("n_qubits", &fp::PauliString::n_qubits)
+      .def_property_readonly("dims", &fp::PauliString::dims)
+      .def_readonly("weight", &fp::PauliString::weight)
+      .def("to_tensor", &fp::PauliString::get_dense_repr<double>)
+      .def(
+          "apply",
+          [](fp::PauliString const &self,
+             std::vector<std::complex<double>> vec) { return self.apply(vec); },
+          "state"_a)
+      .def(
+          "apply_batch",
+          // TODO: this should be handled by proper adapters for mdspan
+          [](fp::PauliString const &self,
+             std::vector<std::vector<std::complex<double>>> inputs,
+             std::complex<double> coef) {
+            // for now we expect row major inputs which have states as columns
+            const size_t n_states = inputs[0].size();
+            std::vector<std::complex<double>> flat_inputs;
+            flat_inputs.reserve(inputs.size() * n_states);
+
+            for (const auto &vec : inputs)
+              if (vec.size() != n_states)
+                throw std::invalid_argument("Bad shape of states array");
+              else
+                std::copy(vec.begin(), vec.end(),
+                          std::back_inserter(flat_inputs));
+
+            std::vector<std::complex<double>> flat_outputs(flat_inputs.size(),
+                                                           0);
+            self.apply_batch(
+                std::mdspan<std::complex<double>, std::dextents<size_t, 2>>{
+                    flat_outputs.data(), inputs.size(), n_states},
+                std::mdspan<std::complex<double>, std::dextents<size_t, 2>>{
+                    flat_inputs.data(), inputs.size(), n_states},
+                coef);
+
+            std::vector<std::vector<std::complex<double>>> results(
+                inputs.size());
+            for (size_t i = 0; i < inputs.size(); i++) {
+              auto it = flat_outputs.begin() + i * n_states;
+              std::copy(it, it + n_states, std::back_inserter(results[i]));
+            }
+            return results;
+          },
+          "states"_a, "coef"_a)
+      .def("__str__",
+           [](fp::PauliString const &self) { return fmt::format("{}", self); });
+
   py::class_<fp::SummedPauliOp<double>>(m, "SummedPauliOp").def(py::init<>());
 }
