81e1ebc87e8be51fd18eabd9036829629275ac89
jamesETsmith <james.smith9113@gmail.com>
2024-09-28 14:30:56 -0400

    [feature/std_exec] Adding std::execution_policy interface for PauliOp

--- fast_pauli/cpp/src/fast_pauli.cpp
+++ fast_pauli/cpp/src/fast_pauli.cpp
@@ -323,7 +323,7 @@ NB_MODULE(_fast_pauli, m)
                      auto states_mdspan = fp::__detail::ndarray_to_mdspan<cfloat_t, 1>(states);
                      auto new_states = fp::__detail::owning_ndarray_like_mdspan<cfloat_t, 1>(states_mdspan);
                      auto new_states_mdspan = std::mdspan(new_states.data(), new_states.size());
-                     self.apply(new_states_mdspan, states_mdspan);
+                     self.apply(std::execution::par, new_states_mdspan, states_mdspan);
                      return new_states;
                  }
                  else if (states.ndim() == 2)
@@ -332,7 +332,7 @@ NB_MODULE(_fast_pauli, m)
                      auto new_states = fp::__detail ::owning_ndarray_like_mdspan<cfloat_t, 2>(states_mdspan);
                      auto new_states_mdspan = fp::__detail::ndarray_to_mdspan<cfloat_t, 2>(new_states);
 
-                     self.apply(new_states_mdspan, states_mdspan);
+                     self.apply(std::execution::par, new_states_mdspan, states_mdspan);
 
                      return new_states;
                  }
@@ -351,7 +351,7 @@ NB_MODULE(_fast_pauli, m)
                      std::array<size_t, 1> out_shape = {1};
                      auto expected_vals_out = fp::__detail::owning_ndarray_from_shape<cfloat_t, 1>(out_shape);
                      auto expected_vals_out_mdspan = fp::__detail::ndarray_to_mdspan<cfloat_t, 1>(expected_vals_out);
-                     self.expectation_value(expected_vals_out_mdspan, states_mdspan_2d);
+                     self.expectation_value(std::execution::par, expected_vals_out_mdspan, states_mdspan_2d);
                      return expected_vals_out;
                  }
                  else if (states.ndim() == 2)
@@ -361,7 +361,7 @@ NB_MODULE(_fast_pauli, m)
                      auto expected_vals_out = fp::__detail::owning_ndarray_from_shape<cfloat_t, 1>(out_shape);
                      auto expected_vals_out_mdspan = fp::__detail::ndarray_to_mdspan<cfloat_t, 1>(expected_vals_out);
 
-                     self.expectation_value(expected_vals_out_mdspan, states_mdspan);
+                     self.expectation_value(std::execution::par, expected_vals_out_mdspan, states_mdspan);
 
                      return expected_vals_out;
                  }
