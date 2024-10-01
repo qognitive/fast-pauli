2e0de65cd239aa227e61b6b5c026f92c4aa51ea2
Sebastien Roy <sebastien.roy@qognitive.io>
2024-10-01 15:28:11 -0400

    _static is added to the list by default

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = []
 
 
 # Breathe configuration
