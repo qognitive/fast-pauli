99e6de7cbbb577fbe8ef1bdb31838a8a4d9318ee
Sebastien Roy <sebastien.roy@qognitive.io>
2024-10-04 15:41:50 -0400

    copyright notice in code

--- fast_pauli/pypauli/__init__.py
+++ fast_pauli/pypauli/__init__.py
@@ -1,3 +1,19 @@
+#############################################################################
+# This code is part of Fast Pauli.
+#
+# (C) Copyright Qognitive Inc 2024.
+#
+# This code is licensed under the BSD 2-Clause License. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+
+#############################################################################
+
 """pypauli module provides implementations to benchmark and test c++ library."""
 
 from fast_pauli.pypauli.pauli_op import PauliOp  # noqa: F401
