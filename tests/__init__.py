9f029d091f32f7a1097a09b9572c215c51181363
Sebastien Roy <sebastien.roy@qognitive.io>
2024-10-04 17:24:32 -0400

    insert copyright in code

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,16 @@
+#############################################################################
+# This code is part of Fast Pauli.
+#
+# (C) Copyright Qognitive Inc 2024.
+#
+# This code is licensed under the BSD 2-Clause License. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+#############################################################################
+
+
 """Test fast_pauli c++ objects against python implementations."""
